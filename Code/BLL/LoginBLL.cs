0c43c3ad4a769c6ff76293e00bac7e0428107b6f
andreclauver <80968256+andreclauver@users.noreply.github.com>
2021-11-19 11:24:04 -0300

    Add files via upload

---
+++ Code/BLL/LoginBLL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Projeto3Camadas.Code.DTO;
+using Projeto3Camadas.Code.DAL;
+using System.Data;
+
+namespace Projeto3Camadas.Code.BLL
+{
+    class LoginBLL
+    {
+        AcessoBancoDados conexao = new AcessoBancoDados();
+        string tabela = "tbl_login";
+
+        public bool RealizarLogin(LoginDTO login)     //Requer: using System.Data;
+        {
+            string sql = $"select * from {tabela} where email='{login.Email}' and senha='{login.Senha}'";
+            DataTable dt = conexao.ExecutarConsulta(sql);
+
+            if (dt.Rows.Count > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public string RetornarSenha(LoginDTO login)     //Requer: using System.Data;
+        {
+            string sql = $"select * from {tabela} where email='{login.Email}'";
+            DataTable dt = conexao.ExecutarConsulta(sql);
+
+            if (dt.Rows.Count > 0)
+                return dt.Rows[0]["senha"].ToString();
+            else
+                return "false";
+        }
+    }
+}
